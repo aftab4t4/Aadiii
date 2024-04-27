2c4505ec9029fbcc72be9bad6d82720566e1ed97
aftab4t4 <168277590+aftab4t4@users.noreply.github.com>
2024-04-27 17:23:51 +0500

    Update YT-search.js

--- commands/YT-search.js
+++ commands/YT-search.js
@@ -98,7 +98,7 @@ _*En cours de téléchargement...*_\n\n`
 
     fileStream.on('finish', () => {
       // Envoi du fichier vidéo en utilisant l'URL du fichier local
-      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *FLASH-MD*", gifPlayback: false }, { quoted: ms });
+      zk.sendMessage(origineMessage, { video: { url: `./${filename}` }, caption: "Powered by *🎗️AFTAB-4T4🎗️*", gifPlayback: false }, { quoted: ms });
 
     });
 
